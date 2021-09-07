a7701fad1a6b09aa764c561b1aba29913bb38abf
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-07 01:11:33 -0300

    add navigation menu

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "@doabit/semantic-ui-sass";
+import "semantic-ui";
+import "@doabit/semantic-ui-sass/src/scss/semantic-ui.scss";
 import Rails from "@rails/ujs";
 import Turbolinks from "turbolinks";
 import * as ActiveStorage from "@rails/activestorage";
 import "channels";
-import "@doabit/semantic-ui-sass";
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+$(".ui.dropdown").dropdown();
