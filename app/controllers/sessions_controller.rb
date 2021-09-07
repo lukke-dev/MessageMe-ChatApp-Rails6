ab6bd1d400649ca8706c3fa0b803708fe67dfe19
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-07 19:49:38 -0300

    add authentication system - create and destroy sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
-  def new
+  def new; end
+
+  def create
+    user = User.find_by(username: params[:session][:username])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = 'Logado com sucesso'
+      redirect_to root_path
+    else
+      flash.now[:error] = 'Usuário ou senha inválidos'
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = 'Deslogado com sucesso'
+    redirect_to login_path
   end
 end
