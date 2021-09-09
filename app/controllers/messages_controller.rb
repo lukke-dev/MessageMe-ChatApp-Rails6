ba5117021bf40c23c2b2cc63fa80449795930532
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-09 03:13:14 -0300

    add messages

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,15 @@
+class MessagesController < ApplicationController
+  before_action :require_user
+
+  def create
+    byebug
+    message = current_user.messages.build(message_params)
+    redirect_to root_path if message.save
+  end
+
+  private
+
+  def message_params
+    params.permit(:body)
+  end
+end
