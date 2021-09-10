dae009b80620a467a3c4e2d1431475d0cb32c4a7
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-10 10:32:30 -0300

    delete route /cable

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   post 'message', to: 'messages#create'
-
-  mount ActionCable.server, at: '/cable'
 end
