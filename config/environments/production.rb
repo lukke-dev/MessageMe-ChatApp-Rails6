df9b21ccf34d1eb642748376806b2b6259736a62
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-10 13:33:32 -0300

    fix error production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -42,7 +42,7 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
+  config.action_cable.url = 'https://coffe-room.herokuapp.com/cable'
   config.action_cable.allowed_request_origins = ['https://coffe-room.herokuapp.com/', %r{https://coffe-room.herokuapp.com/*}]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
