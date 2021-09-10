c70c153b31cd0e9ab3655f870bfb57b7246ebf41
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-09-10 13:33:02 -0300

    fix error production

--- app/controllers/chatroom_controller.rb
+++ app/controllers/chatroom_controller.rb
@@ -2,7 +2,7 @@ class ChatroomController < ApplicationController
   before_action :require_user
 
   def index
-    @messages = Message.all
+    @messages = Message.order(:created_at).last(20)
     @message = Message.new
   end
 end
